184d128188632ae9293557e143869441fb0800d2
jmcooper <jim@cooperplanet.com>
2016-10-18 11:42:22 -0600

    Fix typos

--- app/user/profile.component.ts
+++ app/user/profile.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core'
 
 @Component({
   template: `
-    <h1>Edit Your Profile {{auth.currentUser.firstName}}</h1>
+    <h1>Edit Your Profile</h1>
     <hr>
     <div class="col-md-6">
-      <h3>[Edit profile form will go here]
+      <h3>[Edit profile form will go here]</h3>
       <br />
       <br />
       <button type="submit" class="btn btn-primary">Save</button>
